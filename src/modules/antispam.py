bbfdd255609ea8bd4ffc4d686b0e81420a9da893
A N U S H <54374648+anushkrishnav@users.noreply.github.com>
2021-04-17 20:57:19 +0000

    blocks all link excpet the ones that contain github

--- src/modules/antispam.py
+++ src/modules/antispam.py
@@ -1,9 +1,10 @@
-import random
 from re import search
 links_not_allowed = (829038892145311775,829039782260244561) #moderator only and welcome channel
+allowed_url= "https://github.com/"
 
 def has_links(message):
     url_regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'#\".,<>?«»“”‘’]))"
-    if search(url_regex, message.content) and if message.channel.id in links_not_allowed:
+    if search(url_regex, message.content) and  message.channel.id in links_not_allowed and allowed_url not in message.content :
         return True
     return False
+
