eb82e9c5aecffdb1a71700d55782ca28de032213
A N U S H <54374648+anushkrishnav@users.noreply.github.com>
2021-04-22 13:42:47 +0530

    handler for log

---
+++ src/modules/log.py
@@ -0,0 +1,13 @@
+import logging
+import discord
+
+def create_logger() :
+    """Create a Logger."""
+
+    logger = logging.getLogger("discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
+    logger.addHandler(handler)
+    return logger
